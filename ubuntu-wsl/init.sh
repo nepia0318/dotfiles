8c1ce4388a142c2b32d38f4a73da7ca0546bc719
yuki <nylog0hujikolp@gmail.com>
2024-09-22 23:24:05 +0900

    Test echo shell

--- ubuntu-wsl/init.sh
+++ ubuntu-wsl/init.sh
@@ -12,6 +12,7 @@ printf "\033[32m%s\033[m%s\033[93m%s\033[m%s\n" "[init]" ": " "Begin " "zsh init
 
 sudo apt-get install -y zsh
 sudo chsh "${USER}" -s "$(which zsh)"
+echo "${SHELL}"
 "${FILE_DIR}/zsh/symlink_init.sh"
 # shellcheck source=/dev/null
 source "${HOME}/.zshrc"
