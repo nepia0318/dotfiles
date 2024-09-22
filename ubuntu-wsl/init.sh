af9e5bde929342bdde307b7e037f8eadbafe131a
yuki <nylog0hujikolp@gmail.com>
2024-09-22 23:36:43 +0900

    Add on_zsh

--- ubuntu-wsl/init.sh
+++ ubuntu-wsl/init.sh
@@ -3,7 +3,7 @@
 set -eu
 
 _FILE_DIR=$(cd "$(dirname "$0")" || exit; pwd)
-# readonly FILE_DIR=${_FILE_DIR}
+readonly FILE_DIR=${_FILE_DIR}
 # readonly FILE_DIR_BASE=$(basename ${FILE_DIR})
 printf "\033[32m%s\033[m%s\033[93m%s\033[m%s\n" "[init]" ": " "Begin " "init.sh"
 
@@ -12,36 +12,13 @@ printf "\033[32m%s\033[m%s\033[93m%s\033[m%s\n" "[init]" ": " "Begin " "zsh init
 
 sudo apt-get install -y zsh
 sudo chsh "${USER}" -s "$(which zsh)"
-zsh << 'EOZ'
-echo "${SHELL}"
 "${FILE_DIR}/zsh/symlink_init.sh"
-# shellcheck source=/dev/null
-source "${HOME}/.zshrc"
 
 printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[init]" ": " "Successfully done " "zsh initialization"
 
-# install.sh
-printf "\033[32m%s\033[m%s\033[93m%s\033[m%s\n" "[init]" ": " "Begin " "install.sh"
-
-"${FILE_DIR}/scripts/install.sh"
-
-printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[init]" ": " "Successfully done " "install.sh"
-
-# symlink.sh
-printf "\033[32m%s\033[m%s\033[93m%s\033[m%s\n" "[init]" ": " "Begin " "symlink.sh"
-
-find "${FILE_DIR}" -type f -name 'symlink.sh' -print0 | while IFS= read -r -d '' file; do
-    # basename=$(basename "$file")
-    "$file"
-done
-
-printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[init]" ": " "Successfully done " "symlink.sh"
-
-# shellcheck source=/dev/null
-source "${HOME}/.zshrc"
+# on_zsh
+zsh "${FILE_DIR}/scripts/on_zsh.sh"
 
 # end
-
-EOZ
 
 printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[init]" ": " "Successfully done " "init.sh"
---
+++ ubuntu-wsl/scripts/on_zsh.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -eu
+
+_FILE_DIR=$(cd "$(dirname "$0")" || exit; pwd)
+readonly FILE_DIR=${_FILE_DIR}
+# readonly FILE_DIR_BASE=$(basename ${FILE_DIR})
+
+# zsh
+printf "\033[32m%s\033[m%s\033[93m%s\033[m%s\n" "[on_zsh]" ": " "Begin " "on_zsh.sh"
+
+# shellcheck source=/dev/null
+source "${HOME}/.zshrc"
+
+printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[on_zsh]" ": " "Successfully done " "zsh initialization"
+
+# install.sh
+printf "\033[32m%s\033[m%s\033[93m%s\033[m%s\n" "[on_zsh]" ": " "Begin " "install.sh"
+
+"${FILE_DIR}/scripts/install.sh"
+
+printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[on_zsh]" ": " "Successfully done " "install.sh"
+
+# symlink.sh
+printf "\033[32m%s\033[m%s\033[93m%s\033[m%s\n" "[on_zsh]" ": " "Begin " "symlink.sh"
+
+find "${FILE_DIR}" -type f -name 'symlink.sh' -print0 | while IFS= read -r -d '' file; do
+    # basename=$(basename "$file")
+    "$file"
+done
+
+printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[on_zsh]" ": " "Successfully done " "symlink.sh"
+
+# shellcheck source=/dev/null
+source "${HOME}/.zshrc"
+
+# end
+
+printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[on_zsh]" ": " "Successfully done " "on_zsh.sh"
