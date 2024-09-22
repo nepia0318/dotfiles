08d88041360e3881024a642dd017042111bd0282
yuki <nylog0hujikolp@gmail.com>
2024-09-22 23:51:49 +0900

    Disable unused variable

--- ubuntu-wsl/init.sh
+++ ubuntu-wsl/init.sh
@@ -3,7 +3,7 @@
 set -eu
 
 _FILE_DIR=$(cd "$(dirname "$0")" || exit; pwd)
-readonly FILE_DIR=${_FILE_DIR}
+# readonly FILE_DIR=${_FILE_DIR}
 # readonly FILE_DIR_BASE=$(basename ${FILE_DIR})
 printf "\033[32m%s\033[m%s\033[93m%s\033[m%s\n" "[init]" ": " "Begin " "init.sh"
 
