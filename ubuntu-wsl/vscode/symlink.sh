ead963b3fb9fad9d503c0425706a57c5b48740ff
yuki <nylog0hujikolp@gmail.com>
2024-09-23 04:59:11 +0900

    Fix undefined variable

--- ubuntu-wsl/vscode/symlink.sh
+++ ubuntu-wsl/vscode/symlink.sh
@@ -7,6 +7,8 @@ _FILE_DIR="$(cd "$(dirname "$0")"; pwd)"
 readonly FILE_DIR=$_FILE_DIR
 _FILE_DIR_BASE="$(basename "$FILE_DIR")"
 readonly FILE_DIR_BASE=_FILE_DIR_BASE
+_FILE_NAME="$(basename "$0")"
+readonly FILE_NAME=${_FILE_NAME}
 
 log_success () {
     printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[${FILE_NAME}]" ": " "Successfully done " "$1"
