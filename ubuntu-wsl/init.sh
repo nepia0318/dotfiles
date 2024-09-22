f8781dd8f6c0d9ec41db73b98c4a45e961a7ecae
yuki <nylog0hujikolp@gmail.com>
2024-09-22 22:13:10 +0900

    Fix init.sh2

--- ubuntu-wsl/init.sh
+++ ubuntu-wsl/init.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-readonly FILE_DIR=$(cd $(dirname $0); pwd)
-readonly FILE_DIR_BASE=$(basename ${FILE_DIR})
+_FILE_DIR=$(cd "$(dirname "$0")" || exit; pwd)
+readonly FILE_DIR=$_FILE_DIR
+# readonly FILE_DIR_BASE=$(basename ${FILE_DIR})
 printf "\033[32m%s\033[m%s\033[93m%s\033[m%s\n" "[init]" ": " "Begin " "init.sh"
 
 # zsh
 sudo apt install zsh
 chsh -s /bin/bash
-${FILE_DIR}/zsh/symlink_init.sh
+"${FILE_DIR}/zsh/symlink_init.sh"
 
 # install.sh
-${FILE_DIR}/scripts/install.sh
+"${FILE_DIR}/scripts/install.sh"
 
 # symlink.sh
 find "${FILE_DIR}" -type f -name 'symlink.sh' -print0 | while IFS= read -r -d '' file; do
-    basename=$(basename "$file")
-    zsh $file
+    # basename=$(basename "$file")
+    "$file"
 done
