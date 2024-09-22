c4a09c4d64df39b533b8c05b76a2bf6393cda265
yuki <nylog0hujikolp@gmail.com>
2024-09-22 22:56:36 +0900

    Add logger

--- ubuntu-wsl/init.sh
+++ ubuntu-wsl/init.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 
+set -eu
+
 _FILE_DIR=$(cd "$(dirname "$0")" || exit; pwd)
 readonly FILE_DIR=$_FILE_DIR
 # readonly FILE_DIR_BASE=$(basename ${FILE_DIR})
 printf "\033[32m%s\033[m%s\033[93m%s\033[m%s\n" "[init]" ": " "Begin " "init.sh"
 
 # zsh
+printf "\033[32m%s\033[m%s\033[93m%s\033[m%s\n" "[init]" ": " "Begin " "zsh initialization"
+
 sudo apt-get install -y zsh
 sudo chsh "$USER" -s "$(which zsh)"
 "${FILE_DIR}/zsh/symlink_init.sh"
+
 printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[init]" ": " "Successfully done " "zsh initialization"
 
 # install.sh
+printf "\033[32m%s\033[m%s\033[93m%s\033[m%s\n" "[init]" ": " "Begin " "install.sh"
+
 "${FILE_DIR}/scripts/install.sh"
+
 printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[init]" ": " "Successfully done " "install.sh"
 
 # symlink.sh
+printf "\033[32m%s\033[m%s\033[93m%s\033[m%s\n" "[init]" ": " "Begin " "symlink.sh"
+
 find "${FILE_DIR}" -type f -name 'symlink.sh' -print0 | while IFS= read -r -d '' file; do
     # basename=$(basename "$file")
     "$file"
 done
+
 printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[init]" ": " "Successfully done " "symlink.sh"
 
+# end
 printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[init]" ": " "Successfully done " "init.sh"
--- ubuntu-wsl/scripts/install.sh
+++ ubuntu-wsl/scripts/install.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+set -eu
+
 sudo apt-get install -y build-essential
+printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[install]" ": " "Successfully done " "build-essential"
 sudo apt-get install -y wget
+printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[install]" ": " "Successfully done " "wget"
 
 # golang
 sudo add-apt-repository ppa:longsleep/golang-backports
 sudo apt update
 sudo apt-get install -y golang-go
+printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[install]" ": " "Successfully done " "Go / golang-go"
 
 # Java
 sudo apt-get install -y default-jdk
+printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[install]" ": " "Successfully done " "Java / default-jdk"
 
 # aqua
 mkdir -p "$HOME"/Downloads
@@ -29,6 +35,8 @@ mv "$HOME"/Downloads/aqua "${AQUA_ROOT_DIR:-${XDG_DATA_HOME:-$HOME/.local/share}
 
 aqua i
 
+printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[install]" ": " "Successfully done " "aqua"
+
 # wslu
 sudo apt-get install -y gnupg2 apt-transport-https
 wget -O - https://pkg.wslutiliti.es/public.key | sudo gpg -o /usr/share/keyrings/wslu-archive-keyring.pgp --dearmor
@@ -39,9 +47,14 @@ $(. /etc/os-release && echo "$VERSION_CODENAME") main" | sudo tee /etc/apt/sourc
 sudo apt update
 sudo apt install wslu
 
+printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[install]" ": " "Successfully done " "wslu"
+
 # Rust/Cargo
 curl https://sh.rustup.rs -sSf | sh
+
+printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[install]" ": " "Successfully done " "Rust / Cargo"
 
 # sheldon
 cargo install sheldon
 
+printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[install]" ": " "Successfully done " "sheldon"
