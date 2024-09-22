819c6d012c2b62cc1fb0a2cac2886742c69026f2
yuki <nylog0hujikolp@gmail.com>
2024-09-23 01:10:01 +0900

    Add begin install log

--- ubuntu-wsl/scripts/install.sh
+++ ubuntu-wsl/scripts/install.sh
@@ -2,22 +2,35 @@
 
 set -eu
 
+printf "\033[32m%s\033[m%s\033[93m%s\033[m%s\n" "[install]" ": " "Begin " "build-essential"
 sudo apt-get install -y build-essential
 printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[install]" ": " "Successfully done " "build-essential"
+
+printf "\033[32m%s\033[m%s\033[93m%s\033[m%s\n" "[install]" ": " "Begin " "wget"
+
 sudo apt-get install -y wget
+
 printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[install]" ": " "Successfully done " "wget"
 
 # golang
+printf "\033[32m%s\033[m%s\033[93m%s\033[m%s\n" "[install]" ": " "Begin " "Go / golang-go"
+
 sudo add-apt-repository ppa:longsleep/golang-backports
 sudo apt update
 sudo apt-get install -y golang-go
+
 printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[install]" ": " "Successfully done " "Go / golang-go"
 
 # Java
+printf "\033[32m%s\033[m%s\033[93m%s\033[m%s\n" "[install]" ": " "Begin " "Java / default-jdk"
+
 sudo apt-get install -y default-jdk
+
 printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[install]" ": " "Successfully done " "Java / default-jdk"
 
 # aqua
+printf "\033[32m%s\033[m%s\033[93m%s\033[m%s\n" "[install]" ": " "Begin " "aqua"
+
 mkdir -p "${HOME}/Downloads"
 aqua_url="$( \
     curl -s https://api.github.com/repos/aquaproj/aqua/releases/latest \
@@ -37,6 +50,8 @@ aqua i
 printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[install]" ": " "Successfully done " "aqua"
 
 # wslu
+printf "\033[32m%s\033[m%s\033[93m%s\033[m%s\n" "[install]" ": " "Begin " "wslu"
+
 sudo apt-get install -y gnupg2 apt-transport-https
 wget -O - https://pkg.wslutiliti.es/public.key | sudo gpg -o /usr/share/keyrings/wslu-archive-keyring.pgp --dearmor
 # shellcheck source=/dev/null
@@ -49,11 +64,15 @@ sudo apt install wslu
 printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[install]" ": " "Successfully done " "wslu"
 
 # Rust/Cargo
+printf "\033[32m%s\033[m%s\033[93m%s\033[m%s\n" "[install]" ": " "Begin " "Rust / Cargo"
+
 curl https://sh.rustup.rs -sSf | sh
 
 printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[install]" ": " "Successfully done " "Rust / Cargo"
 
 # sheldon
+printf "\033[32m%s\033[m%s\033[93m%s\033[m%s\n" "[install]" ": " "Begin " "sheldon"
+
 cargo install sheldon
 
 printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[install]" ": " "Successfully done " "sheldon"
