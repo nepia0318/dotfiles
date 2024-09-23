502ffde76f55a9f878cec7748680acbf44ac9ae8
nepia <nepia0318@email.com>
2024-09-23 21:24:09 +0900

    Fix symlink for gitconfig

--- ubuntu-wsl/git/symlink.sh
+++ ubuntu-wsl/git/symlink.sh
@@ -14,40 +14,16 @@ log_success () {
     printf "\033[32m%s\033[m%s\033[96m%s\033[m%s\n" "[${FILE_NAME}]" ": " "Successfully done " "$1"
 }
 
-# .zshrc
-TARGET_ZSHRC="${HOME}/.zshrc"
+# .gitconfig
+TARGET_GITCONFIG="${HOME}/.gitconfig"
 
-mkdir -p "$(dirname "$TARGET_ZSHRC")"
+mkdir -p "$(dirname "$TARGET_GITCONFIG")"
 
-if [ -e "${TARGET_ZSHRC}" ]; then
-    cp "${TARGET_ZSHRC}" "${TARGET_ZSHRC}_backup"
+if [ -e "${TARGET_GITCONFIG}" ]; then
+    cp "${TARGET_GITCONFIG}" "${TARGET_GITCONFIG}_backup"
 fi
 
-ln -fns "${FILE_DIR}/.zshrc" "${TARGET_ZSHRC}"
-
-# target base
-TARGET_BASE="${HOME}/.zsh"
-EXCEPT_FILES=("$(basename "$0")" ".zshrc")
-
-find "$FILE_DIR" -type f -name '???*' -print0 | while IFS= read -r -d '' file; do
-    basename="$(basename "$file")"
-    for except_file in "${EXCEPT_FILES[@]}"; do
-        if [[ $basename == "$except_file" ]]; then
-            continue 2
-        fi
-    done
-
-    rel_path=${file#"${FILE_DIR}"/}
-    target_dir="${TARGET_BASE}/$(dirname "$rel_path")"
-    mkdir -p "$target_dir"
-
-    if [ -e "${TARGET_BASE}/${rel_path}" ]; then
-        cp "${TARGET_BASE}/${rel_path}" "${TARGET_BASE}/${rel_path}_backup"
-    fi
-
-    ln -fns "$file" "${TARGET_BASE}/${rel_path}"
-done
-
+ln -fns "${FILE_DIR}/.gitconfig" "${TARGET_GITCONFIG}"
 
 # sucess
 log_success "${FILE_DIR_BASE}"
