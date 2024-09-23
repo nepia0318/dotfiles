7aaf3745a47a022eae5786fe9ad953b1384f2931
nepia <nepia0318@email.com>
2024-09-23 21:21:13 +0900

    Remove blank line

--- ubuntu-wsl/scripts/vscode_install.sh
+++ ubuntu-wsl/scripts/vscode_install.sh
@@ -19,13 +19,11 @@ log_success () {
 
 log_begin "vscode_install"
 
-
 # apt install
 while read -r line
 do
     code --install-extension "${line}"
     log_success "vscode/${line}"
 done < "${FILE_DIR}/../vscode/extensions.txt"
-
 
 log_success "vscode_install"
